42061e85be4a0fa5d7d9051ba56edc2891134f00
Finn Morrison <fmorrison3@gatech.edu>
2021-10-23 20:21:36 -0400

    DBObject for Sully

--- src/main/java/com/healthhgt8/mentalhealthapp/DBObject.java
+++ src/main/java/com/healthhgt8/mentalhealthapp/DBObject.java
@@ -4,23 +4,48 @@ import com.mongodb.ConnectionString;
 import com.mongodb.MongoClientSettings;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import org.w3c.dom.Document;
 
+
+import java.util.HashMap;
+
+
+// FIXME: NEED TO IMPORT org.bson.Document or something along those lines
 public class DBObject {
-    // make as a singleton- we only need one instance of this, one client etc.
-    private MongoDatabase db;
-    private MongoClient client;
-    private String var3;
 
-    /**
-     *  cAL
-     * @param connectionString
-     */
-    public DBObject(ConnectionString connectionString) {
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://hackgt8-mental-health:@cluster0.j9dny.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
-        MongoClientSettings settings = MongoClientSettings.builder()
-        .applyConnectionString(connectionString)
-        .build();
-        MongoClient mongoClient = MongoClients.create(settings);
+    private static DBObject instance = new DBObject();
+    private MongoClient client = MongoClients.create(""); //FIXME: paste in connection string here
+
+
+    private DBObject() {
+
+    }
+
+    // Get the only object available
+    public static DBObject getInstance() {
+        if (instance == null) {
+            instance = new DBObject();
+        }
+        return instance;
+    }
+
+    // can't ensure the type of all values in this map, but the keys should be strings
+    public boolean sendToDB(HashMap<String, Object> BSONKeysAndValues, String dbName, String collectionName) {
+        Document document = new Document();
+        for (String key: BSONFieldsAndValues.keySet()) {
+            document.append(key, value);
+        }
+        MongoDatabase db = client.getDatabase(dbName);
+        //Inserting the document into the collection
+        db.getCollection(collectionName).insertOne(document);
+        System.out.println("Document inserted successfully");
+
+    }
+
+
+    public MongoClient getClient() {
+        return this.client;
     }
 }
